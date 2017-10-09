189cfb436cedb1bba97405c0c79c789dfce4702c
ArafatUWL <32455754+ArafatUWL@users.noreply.github.com>
2017-10-09 13:29:04 +0100

    Add files via upload

---
+++ idllesession4.py
@@ -0,0 +1,15 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/Users/21358237/Desktop/idlesession4.py =============
+Radius:2
+Traceback (most recent call last):
+  File "C:/Users/21358237/Desktop/idlesession4.py", line 4, in <module>
+    area = pix**2
+NameError: name 'pix' is not defined
+>>> 
+>>> 
+============= RESTART: C:/Users/21358237/Desktop/idlesession4.py =============
+Radius: 2
+30.959144000000002
+>>> 
